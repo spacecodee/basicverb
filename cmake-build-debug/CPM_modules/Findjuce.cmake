5f1d635d1730a7a15159459581a8c3b07f837ef1
spacecodee <spacecodee@gmail.com>
2025-03-02 11:31:32 -0500

    Add

--- cmake-build-debug/CPM_modules/Findjuce.cmake
+++
@@ -1,3 +0,0 @@
-include("/Users/ronmac/CLionProjects/BasicVerb/cmake-build-debug/cmake/CPM_0.38.2.cmake")
-CPMAddPackage("NAME;juce;GIT_REPOSITORY;https://github.com/juce-framework/JUCE.git;GIT_TAG;origin/master")
-set(juce_FOUND TRUE)
