7c88a2cd1acc9de47dfdccf782019be641053861
spacecodee <spacecodee@gmail.com>
2025-03-02 12:19:20 -0500

    Add sliders and labels for audio parameters in the plugin editor

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -3,7 +3,8 @@
 #include "PluginProcessor.h"
 
 //==============================================================================
-class AudioPluginAudioProcessorEditor final : public juce::AudioProcessorEditor
+class AudioPluginAudioProcessorEditor final
+    : public juce::AudioProcessorEditor
 {
 public:
     explicit AudioPluginAudioProcessorEditor(AudioPluginAudioProcessor&);
@@ -17,6 +18,24 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     AudioPluginAudioProcessor& processorRef;
+
+    juce::Slider roomSizeSlider;
+    juce::Slider dampingSlider;
+    juce::Slider wetLevelSlider;
+    juce::Slider dryLevelSlider;
+    juce::Slider widthSlider;
+    juce::Slider freezeModeSlider;
+
+    juce::Label roomSizeLabel;
+    juce::Label dampingLabel;
+    juce::Label wetLevelLabel;
+    juce::Label dryLevelLabel;
+    juce::Label widthLabel;
+    juce::Label freezeModeLabel;
+
+    std::vector<
+        std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment>>
+    sliderAttachments;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AudioPluginAudioProcessorEditor)
 };
