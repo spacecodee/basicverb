aaaa16066ab36bd7bc8f067c0b62f97f448358d0
spacecodee <spacecodee@gmail.com>
2025-03-02 11:30:13 -0500

    First Commit

---
+++ cmake-build-debug/CPM_modules/Findjuce.cmake
@@ -0,0 +1,3 @@
+include("/Users/ronmac/CLionProjects/BasicVerb/cmake-build-debug/cmake/CPM_0.38.2.cmake")
+CPMAddPackage("NAME;juce;GIT_REPOSITORY;https://github.com/juce-framework/JUCE.git;GIT_TAG;origin/master")
+set(juce_FOUND TRUE)
